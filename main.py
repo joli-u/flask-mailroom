546bf137809046342242e950d77c020376044d00
Joli Umetsu <joliumetsu@gmail.com>
2020-05-16 20:53:33 -0700

    initial commit.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -11,13 +11,32 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
 
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = (request.form['name']).title()
+        value = int(request.form['amount'])
+        
+        try:
+            donor = Donor.get(Donor.name == name)
+
+        except Donor.DoesNotExist:
+            return render_template('create.jinja2', error="Name not found.")
+
+        Donation(value=value, donor=donor).save()
+        return redirect(url_for('all'))
+
+    else:
+        return render_template('create.jinja2')
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
